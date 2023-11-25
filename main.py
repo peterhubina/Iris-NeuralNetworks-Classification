1a87f7b991a9373154bdc204fb5bb3bab684cc8b
peterhubina <peter.hubina1@gmail.com>
2023-11-25 18:20:01 +0100

    Update

--- main.py
+++ main.py
@@ -1,16 +1,133 @@
-from sklearn import datasets
 import torch
-import pandas as pd
+import torch.nn as nn
+import torch.optim as optim
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from torch.utils.data import DataLoader, TensorDataset
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Load the Iris dataset
-iris = datasets.load_iris()
-X, y = iris.data, iris.target
+iris = load_iris()
+X = iris.data
+y = iris.target
 
-# Create a DataFrame with feature data
-iris_df = pd.DataFrame(X, columns=iris.feature_names)
+# Inspect the dataset
+print("Dataset shape:", X.shape)
+print("Labels:", set(y))
 
-# Add a column for the target (species)
-iris_df['species'] = pd.Categorical.from_codes(y, iris.target_names)
+# Step 2. : Data preprocessing
 
-# Display the first few rows of the DataFrame
-print(iris_df.head())
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Function to create scatter plots
+def create_scatter_plot(X, y, features, title):
+    # Set up the figure and axis
+    plt.figure(figsize=(12, 10))
+    plt.suptitle(title, fontsize=16)
+
+    # Create a scatter plot for each pair of features
+    plot_number = 1
+    for i in range(len(features)):
+        for j in range(len(features)):
+            if i < j:
+                plt.subplot(2, 3, plot_number)
+                for target in np.unique(y):
+                    subset = X[y == target]
+                    plt.scatter(subset[:, i], subset[:, j], label=iris.target_names[target])
+                plt.xlabel(features[i])
+                plt.ylabel(features[j])
+                plt.legend()
+                plot_number += 1
+
+    plt.show()
+
+# Visualize the training set
+create_scatter_plot(X_train, y_train, iris.feature_names, 'Iris Dataset - Training Set')
+
+# Visualize the testing set
+create_scatter_plot(X_test, y_test, iris.feature_names, 'Iris Dataset - Testing Set')
+
+# Standardize the dataset
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Convert to PyTorch tensors
+X_train = torch.tensor(X_train, dtype=torch.float32)
+X_test = torch.tensor(X_test, dtype=torch.float32)
+y_train = torch.tensor(y_train, dtype=torch.long)
+y_test = torch.tensor(y_test, dtype=torch.long)
+
+# Create data loaders
+train_dataset = TensorDataset(X_train, y_train)
+test_dataset = TensorDataset(X_test, y_test)
+
+train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)
+
+# Neural network architecture
+
+
+class IrisNet(nn.Module):
+    def __init__(self):
+        super(IrisNet, self).__init__()
+        self.fc1 = nn.Linear(4, 10)
+        self.fc2 = nn.Linear(10, 10)
+        self.fc3 = nn.Linear(10, 3)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+model = IrisNet()
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+# Training loop
+for epoch in range(100):
+    for data in train_loader:
+        inputs, labels = data
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+    print(f'Epoch {epoch+1}, Loss: {loss.item()}')
+
+
+def evaluate_model(model, data_loader):
+    model.eval()
+    total = 0
+    correct = 0
+    with torch.no_grad():
+        for data in data_loader:
+            inputs, labels = data
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    accuracy = 100 * correct / total
+    return accuracy
+
+accuracy = evaluate_model(model, test_loader)
+print(f'Accuracy: {accuracy}%')
+
+sample_inputs = torch.tensor([[5.1, 3.5, 1.4, 0.2], [6.7, 3.0, 5.2, 2.3]], dtype=torch.float32)
+sample_inputs = scaler.transform(sample_inputs)  # Apply the same scaling as the training data
+sample_inputs = torch.tensor(sample_inputs, dtype=torch.float32)
+
+model.eval()
+with torch.no_grad():
+    sample_outputs = model(sample_inputs)
+    _, predicted = torch.max(sample_outputs, 1)
+print("Predictions:", predicted.numpy())
+
+
+
